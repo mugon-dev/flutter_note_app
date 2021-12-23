db5256d4b997c6f3a7788ac7588c2e24752f6446
jmg <jmg41490400@gmail.com>
2021-12-23 10:53:44 +0900

    create note page

--- lib/presentation/notes/notes_screen.dart
+++ lib/presentation/notes/notes_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_note_app/domain/model/note.dart';
+import 'package:flutter_note_app/presentation/add_edit_note/add_edit_note_screen.dart';
+import 'package:flutter_note_app/presentation/notes/components/note_item.dart';
+import 'package:flutter_note_app/ui/colors.dart';
 
 class NotesScreen extends StatelessWidget {
   const NotesScreen({Key? key}) : super(key: key);
@@ -15,10 +19,38 @@ class NotesScreen extends StatelessWidget {
           IconButton(onPressed: () {}, icon: const Icon(Icons.sort)),
         ],
       ),
-      body: Container(),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => AddEditNoteScreen(),
+              ));
+        },
         child: const Icon(Icons.add),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8),
+        child: ListView(
+          children: [
+            NoteItem(
+              note: Note(
+                title: 'title 1',
+                content: 'content 1',
+                color: wisteria.value,
+                timestamp: 1,
+              ),
+            ),
+            NoteItem(
+              note: Note(
+                title: 'title 2',
+                content: 'content 2',
+                color: skyBlue.value,
+                timestamp: 1,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
